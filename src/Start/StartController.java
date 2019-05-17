5a1eac9224e5fb0f2c146f8edb65a4ac15ba81cc
AliceGajos <alice@REVDEBUG.COM>
2019-05-17 13:50:39 +0200

    Ftr: Hide Show PASS

--- src/Start/StartController.java
+++ src/Start/StartController.java
@@ -8,14 +8,25 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.CheckBox;
 
 public class StartController {
-    public void ShowPass_Changed(ActionEvent actionEvent) {
-        //TODO Mechanism showing password
-        if(showPassCheckBox.isSelected()) passwordFieldLogin.setVisible(true);
 
-    }
     @FXML private TextField emailFieldLogin;
     @FXML private PasswordField passwordFieldLogin;
     @FXML private TextField passwordTextFieldLogin;
     @FXML private Button loginButton;
     @FXML private CheckBox showPassCheckBox;
+
+    //This method is hiding and showing password
+    public void ShowPass_Changed(ActionEvent actionEvent) {
+        //Mechanism showing password
+        if(showPassCheckBox.isSelected()) {
+            passwordTextFieldLogin.setText(passwordFieldLogin.getText());
+            passwordTextFieldLogin.setVisible(true);
+        }
+        else {
+            passwordFieldLogin.setText(passwordTextFieldLogin.getText());
+            passwordTextFieldLogin.setVisible(false);
+
+        }
+
+    }
 }
